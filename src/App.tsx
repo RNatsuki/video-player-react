587ead743cefc2b93e0d942bfd81620f9e6f6a4f
Ibarra <ibarra2002x3@gmail.com>
2024-09-28 08:56:08 -0600

    Refactor package.json to update project name, version, and author information

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import VideoPlayer from "./components/VideoPlayer";
+/* import VideoPlayer from "./components/VideoPlayer";
 import video from "./videos/sofi.mp4";
-import thumbnail from "./thumbnails/image.png";
+import thumbnail from "./thumbnails/image.png"; */
 const App = () => {
   return (
     <div className="content">
       <h1>SofiDev Video Player</h1>
-      <VideoPlayer src={video} thumbnail={thumbnail} />
+      {/* <VideoPlayer src={video} thumbnail={thumbnail} /> */}
     </div>
   );
 };
